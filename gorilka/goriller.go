164a3d7be11a66ab94474d191bddc54eb9fd9b88
Naeel <tazet@narod.ru>
2024-12-18 09:13:06 +0300

    both

--- gorilka/goriller.go
+++ gorilka/goriller.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -12,14 +16,9 @@ const localPort = "localhost:8080"
 
 func main() {
 	router := mux.NewRouter()
-	// router.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-	// 	vars := mux.Vars(req)
-	// 	fmt.Fprintf(w, "Welcome %v\n", vars)
-	// 	fmt.Println("Welcome to the home page!!!!!")
-	// }).Methods("GET")
-	router.Headers("Content-Type", "text/plain")
+	router.Headers("Content-Type", "application/json")
+
 	router.HandleFunc("/params", params).Methods("POST")
-	//	router.Headers("Content-Type", "application/json")
 	//	router.HandleFunc("/", treat).Methods("POST")
 
 	if err := http.ListenAndServe(localPort, router); err != nil {
@@ -28,27 +27,48 @@ func main() {
 }
 
 func params(rwr http.ResponseWriter, req *http.Request) {
+	rwr.Header().Set("Content-Encoding", "gzip")
+	rwr.Header().Set("Content-Type", "application/javascript")
 	telo, err := io.ReadAll(req.Body)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Fprintf(rwr, "string(body) --- >  %v\n", string(telo))
-	hdr := req.Header
-	fmt.Fprintf(rwr, "headers %[1]v type %[1]T\n", hdr)
-	hst := req.Host
-	fmt.Fprintf(rwr, "Host %[1]v type %[1]T\n", hst)
-	frm := req.Form
-	fmt.Fprintf(rwr, "Form %[1]v type %[1]T\n", frm)
-	resp := req.Response
-	fmt.Fprintf(rwr, "Response %[1]v type %[1]T\n", resp)
-	meth := req.Method
-	fmt.Fprintf(rwr, "Method %[1]v type %[1]T\n", meth)
-	requ := req.RequestURI
-	fmt.Fprintf(rwr, "RequestURI %[1]v type %[1]T\n", requ)
-	url := req.URL
-	fmt.Fprintf(rwr, "URL %[1]v type %[1]T\n", url)
 
-	//fmt.Fprintf(rwr, "%v", ae)
-	//	rwr.Write([]byte(ae))
+	fmt.Printf("Accept-Encoding  %v\n", req.Header.Get("Accept-Encoding"))
+	fmt.Printf("Content-Encoding  %v\n", req.Header.Get("Content-Encoding"))
+	// hdr := req.Header
+	// fmt.Fprintf(rwr, "headers %[1]v type %[1]T\n", hdr)
+	// hst := req.Host
+	// fmt.Fprintf(rwr, "Host %30[1]v type %[1]T\n", hst)
+	// frm := req.Form
+	// fmt.Fprintf(rwr, "Form %30[1]v type %[1]T\n", frm)
+	// resp := req.Response
+	// fmt.Fprintf(rwr, "Response %30[1]v type %[1]T\n", resp)
+	// meth := req.Method
+	// fmt.Fprintf(rwr, "Method %30[1]v type %[1]T\n", meth)
+	// requ := req.RequestURI
+	// fmt.Fprintf(rwr, "RequestURI %30[1]v type %[1]T\n", requ)
+	// url := req.URL
+	// fmt.Fprintf(rwr, "URL %30[1]v type %[1]T\n", url)
+	// zippers := req.Header.Get("Accept-Encoding")
+	// fmt.Fprintf(rwr, "Zippers %30[1]v type %[1]T\n", zippers)
+	// fmt.Fprintf(rwr, "Compressed %[1]v type %[1]T\n", buf)
+
+	//	gzip.NewWriter(w io.Writer)
+	var buf bytes.Buffer
+	zw := gzip.NewWriter(&buf)
+	zw.Name = "zw name"
+	zw.Comment = "zw comment"
+	zw.ModTime = time.Now()
+	_, err = zw.Write(telo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := zw.Close(); err != nil {
+		log.Fatal(err)
+	}
+	rwr.Write(buf.Bytes())
+
+	//		fmt.Printf("Name: %s\nComment: %s\nModTime: %s\n\n", zr.Name, zr.Comment, zr.ModTime.UTC())
 
 }
--- senders/sendpost.go
+++ senders/sendpost.go
@@ -2,69 +2,112 @@ package main
 
 import (
 	"bytes"
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	//"strings"
 )
 
 var host = "localhost:8080"
 
-func postByNewRequest() error {
+// type decomp struct {
+// 	size int
+// 	head gzip.Header
+// }
 
-	jsonStr := `{"a":7}`
-	jsonStrM, _ := json.Marshal(jsonStr)
-	//req, err := http.NewRequest("POST", "http://localhost:8080/s", nil)
-	//	req, err := http.NewRequest("POST", "http://localhost:8080/s", strings.NewReader("zalupan"))
-	req, err := http.NewRequest("POST", "http://localhost:8080/s", bytes.NewBuffer(jsonStrM))
+func postJSONByNewRequest(jsonStr string) (*gzip.Reader, error, int) {
+
+	jsonStrMarshalled, err := json.Marshal(jsonStr)
 	if err != nil {
-		return fmt.Errorf("erra http.NewRequest %w ", err)
+		return nil, fmt.Errorf("marshal err %w ", err), 0
 	}
-	//req.Header.Set("Content-Type", "text/plain")
-	req.Header.Set("Content-Type", "application/json")
-	//	req.Header.Set("Content-Encoding", "gzip")
+	requerest, err := http.NewRequest("POST", "http://"+host+"/params", bytes.NewBuffer(jsonStrMarshalled))
+	if err != nil {
+		return nil, fmt.Errorf("erra http.NewRequest %w ", err), 0
+	}
+	fmt.Println("Header ", requerest.Header)
+	//	requerest.Header.Set("Content-Type", "application/json")
+	requerest.Header.Set("Accept-Encoding", "gzip")
+	requerest.Header.Set("Content-Encoding", "gzip;zalupan")
 
 	client := &http.Client{}
 
-	res, err := client.Do(req)
+	responsa, err := client.Do(requerest)
 	if err != nil {
-		return fmt.Errorf("client.Do http.NewRequest %w ", err)
+		return nil, fmt.Errorf("client.Do  %w ", err), 0
 	}
-	defer res.Body.Close()
+	defer responsa.Body.Close()
 
-	body1, err := io.ReadAll(res.Body)
+	dlina, errD := strconv.Atoi(responsa.Header.Get("Content-Length"))
+	if errD != nil {
+		return nil, fmt.Errorf("strconv.Atoi(responsa.Header.Get(\"Content-Length\")) %w ", errD), 0
+	}
+
+	gzipReader, err := gzip.NewReader(responsa.Body)
 	if err != nil {
-		return fmt.Errorf("erra io.ReadAll %w ", err)
+		return nil, fmt.Errorf("gzip.NewReader %w ", err), 0
 	}
-	fmt.Printf("body ...%v ...\n", string(body1))
 
-	return nil
+	if err := gzipReader.Close(); err != nil {
+		return nil, fmt.Errorf("zr.Close %w ", err), 0
+	}
+
+	return gzipReader, nil, dlina
 }
 func main() {
-	//	stat := postMetric("1", "2", "3")
-	err := postByPost()
+	metr := `{"a":7,"b":"1234a"}`
+	//	err := postByPost(metr)
+	outer, err, dlina := postJSONByNewRequest(metr)
 	if err != nil {
 		log.Fatalf("fatalled postmetric -->\n%v\n<---\n", err)
 	}
-	fmt.Println("status ", err)
-	//		panic(err)
+	//outer.
+	decompressedData := make([]byte, dlina)
+	_, err = outer.Read(decompressedData)
+	if err != nil && err != io.EOF {
+		fmt.Println("Ошибка чтения декомпрессированных данных:", err)
+		return
+	}
 
+	fmt.Printf("%v", string(decompressedData))
+	// if _, err := io.Copy(os.Stdout, outer); err != nil {
+	// 	log.Fatal(err)
+	// }
 }
 
-func postByPost() error {
-	metr := `{"a":7,"b":"1234a"}`
+func postByPost(metr string) error {
+
 	march, _ := json.Marshal(metr)
-	resp, err := http.Post("http://"+host+"/params", "application/json", bytes.NewBuffer(march))
+	respon, err := http.Post("http://"+host+"/params", "application/json", bytes.NewBuffer(march))
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
-	body1, err := io.ReadAll(resp.Body)
+	defer respon.Body.Close()
+
+	//	fmt.Printf("bashka %v\n", respon.Header)
+
+	respon.Header.Set("Accept-Encoding", "gzip")
+
+	zr, err := gzip.NewReader(respon.Body)
 	if err != nil {
-		return fmt.Errorf("erra io.ReadAll %w ", err)
+		log.Fatal(err)
 	}
-	fmt.Printf("body ...%v ...\n", string(body1))
+
+	if written, err := io.Copy(os.Stdout, zr); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("\nWritten %d\nName: %s\nComment: %s\nModTime: %s\n\n", written, zr.Name, zr.Comment, zr.ModTime.UTC())
+	}
+	fmt.Printf("Header %v", respon.Header)
+	// body1, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	return fmt.Errorf("erra io.ReadAll %w ", err)
+	// }
+	// fmt.Printf("body ...%v ...\n", string(body1))
 	return nil
 }
