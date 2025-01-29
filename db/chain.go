029e03a9d13096d3f2e5eada13c1670c1913b5c6
Naeel <tazet@narod.ru>
2025-01-29 15:32:01 +0300

    ok

--- db/chain.go
+++ db/chain.go
@@ -6,21 +6,42 @@ type chaina struct {
 	ch chan int
 }
 
-
-func (s chaina)  add() chan int {
-	chacha := make(chan int)
+func (s chaina) add2() chaina {
+	chacha := chaina{ch: make(chan int)}
 	go func() {
-		defer close(chacha)
+		defer close(chacha.ch)
 		for value := range s.ch {
-			chacha <- value
+			chacha.ch <- value + 2
 		}
+	}()
+	return chacha
+}
+func (s chaina) add3() chaina {
+	chacha := chaina{ch: make(chan int)}
+	go func() {
+		defer close(chacha.ch)
+		for value := range s.ch {
+			chacha.ch <- value + 3
+		}
+	}()
+	return chacha
+}
+func (s chaina) summ() chaina {
+	chacha := chaina{ch: make(chan int)}
+	go func() {
+		defer close(chacha.ch)
+		sum := 0
+		for value := range s.ch {
+			sum += value
+		}
+		chacha.ch <- sum
 	}()
 	return chacha
 }
 
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
-	s := chaina{ch:make(chan int) }
+	s := chaina{ch: make(chan int)}
 
 	go func() {
 		defer close(s.ch)
@@ -29,9 +50,9 @@ func main() {
 		}
 	}()
 
-	addCh := s.add()
-	
-	for res := range addCh {
+	addCh := s.add2().add3().summ()
+
+	for res := range addCh.ch {
 		fmt.Print(res, " ")
 	}
 }
