de0b63e74b9f797fe6bd1cfd153e158fa6bb5e28
Naeel <tazet@narod.ru>
2025-01-29 15:06:01 +0300

    nogen

--- db/chain.go
+++ db/chain.go
@@ -26,8 +26,16 @@ func generator(numbers []int) chan int {
 
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
+	inputCh := make(chan int)
+	go func() {
+		defer close(inputCh)
+		for _, num := range numbers {
+			inputCh <- num
+		}
+	}()
 
-	inputCh := generator(numbers)
+	//inputCh := generator(numbers)
+
 	addCh := add(inputCh)
 	for res := range addCh {
 		fmt.Print(res, " ")
