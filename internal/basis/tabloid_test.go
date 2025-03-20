d9d9743284d5f8f8f2b7ef44f34b81d7fe450e38
Naeel <tazet@narod.ru>
2025-03-20 17:12:05 +0400

    Test02DBstruct_GetMetric

--- internal/basis/tabloid_test.go
+++ internal/basis/tabloid_test.go
@@ -2,6 +2,7 @@ package basis
 
 import (
 	"context"
+	"encoding/json"
 	"gorono/internal/middlas"
 	"gorono/internal/models"
 )
@@ -49,7 +50,7 @@ func (suite *TstBase) Test00InitDBStorage() {
 	}
 }
 
-func (suite *TstBase) TestDBstruct_PutMetric() {
+func (suite *TstBase) Test01DBstruct_PutMetric() {
 
 	tests := []struct {
 		name    string
@@ -61,16 +62,66 @@ func (suite *TstBase) TestDBstruct_PutMetric() {
 			metr:    models.Metrics{MType: "gauge", ID: "Alloc", Value: middlas.Ptr(777.77)},
 			wantErr: false,
 		},
-		// {
-		// 	name:    "Nice counter PUT metric",
-		// 	metr:    models.Metrics{MType: "counter", ID: "coooo", Delta: middlas.Ptr[int64](777)},
-		// 	wantErr: false,
-		// },
+		{
+			name:    "Nice counter PUT metric",
+			metr:    models.Metrics{MType: "counter", ID: "coooo", Delta: middlas.Ptr[int64](777)},
+			wantErr: false,
+		},
+		{
+			name:    "Wrong TYPE",
+			metr:    models.Metrics{MType: "WTFtype", ID: "nocoooo", Delta: middlas.Ptr[int64](777)},
+			wantErr: true,
+		},
+		{
+			name:    "Wrong value instead DELTA",
+			metr:    models.Metrics{MType: "counter", ID: "ooo", Value: middlas.Ptr(777.88)},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		suite.Run(tt.name, func() {
 			err := suite.dataBase.PutMetric(suite.ctx, &tt.metr, nil)
 			suite.Require().Equal(err != nil, tt.wantErr)
+		})
+	}
+}
+func (suite *TstBase) Test02DBstruct_GetMetric() {
+
+	tests := []struct {
+		name          string
+		metr, gotmetr Metrics
+		wantErr       bool
+	}{
+		{
+			name:    "Nice gauge GET metric",
+			metr:    models.Metrics{MType: "gauge", ID: "Alloc"},
+			gotmetr: models.Metrics{MType: "gauge", ID: "Alloc", Value: middlas.Ptr(777.77)},
+			wantErr: false,
+		},
+		{
+			name:    "Nice counter GET metric",
+			metr:    models.Metrics{MType: "counter", ID: "coooo"},
+			gotmetr: models.Metrics{MType: "counter", ID: "coooo", Delta: middlas.Ptr[int64](777)},
+			wantErr: false,
+		},
+		{
+			name:    "bad GET, no ID",
+			metr:    models.Metrics{MType: "counter", ID: "c"},
+			gotmetr: models.Metrics{MType: "counter", ID: "c", Delta: middlas.Ptr[int64](777)},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		suite.Run(tt.name, func() {
+			err := suite.dataBase.GetMetric(suite.ctx, &tt.metr, nil)
+			suite.Require().Equal(err != nil, tt.wantErr)
+			if err == nil {
+				gm, _ := json.Marshal(tt.gotmetr)
+				gm1, _ := json.Marshal(tt.metr)
+
+				//	reflect.DeepEqual(m1, m2)
+				suite.Require().JSONEq(string(gm), string(gm1))
+			}
 		})
 	}
 }
